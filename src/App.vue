df5b6d1b48c313e579ab7b6839b8640cef08ea09
DESKTOP-U0S2FK9\DEOKHWAN <dhdh9224@gmail.com>
2021-10-22 22:57:43 +0900

    기본 Vue 설정

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-link to="/">Home</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+#app {
+  font-size: 18px;
+  font-family: 'Roboto', sans-serif;
+  color: blue;
+}
+</style>
